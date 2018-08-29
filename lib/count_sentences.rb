ac947853eb1148dc7b6e5a6167e5f71d5d8d390e
Jackie Ha <jha2@wellesley.edu>
2018-08-29 15:54:02 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -34,6 +34,6 @@ class String
     # count number of elements from split
     # 
     # self.split if self.end_with?(". ") || self.end_with?("? ") || self.end_with?("! ")
-    self.count_sentences
+    self.sentences
   end
 end 
